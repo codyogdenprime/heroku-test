ee03027b9f69cae526818c2be6d75b6e2d495093
Cody Ogden <cody@codyogden.com>
2016-09-09 07:38:19 -0500

    another try

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require( 'express' );
 var app = express();
 
-app.listen(80, "localhost");
+app.set('port', (process.env.PORT || 5000));
 
 app.use( express.static( 'public', { extended: false } ));
 
 app.get( '/', function( req, res ) {
 	res.send( 'Hello, Heroku!' );
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
