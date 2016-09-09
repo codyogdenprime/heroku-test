b072a38d771dda3edff86a8f79b641e8143d6375
Cody Ogden <cody@codyogden.com>
2016-09-09 06:32:37 -0500

    80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require( 'express' );
 var app = express();
 
-app.listen(3000, "localhost");
+app.listen(80, "localhost");
 
 app.get( '/', function( req, res ) {
 	res.send( 'Hello, Heroku!' );
